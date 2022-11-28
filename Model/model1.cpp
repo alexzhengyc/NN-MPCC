4e9c9cad6a15b45a4cc34af034410c7d150ddfa9
AlexZheng-UCLA <alexzheng1017@gmail.com>
2022-11-27 17:10:47 -0800

    semi-final

--- Model/model1.cpp
+++ Model/model1.cpp
@@ -1,4 +1,4 @@
-// Copyright 2019 Alexander Liniger
+// Copyright 2022 Alex Zheng
 
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
